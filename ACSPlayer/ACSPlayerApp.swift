988812a4af7adda9cc0e663e1da315af974af6ae
isaacd2 <idarr08@gmail.com>
2024-01-20 01:12:50 -0500

    Initial Commit

---
+++ ACSPlayer/ACSPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACSPlayerApp.swift
+//  ACSPlayer
+//
+//  Created by Isaac D2 on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ACSPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
